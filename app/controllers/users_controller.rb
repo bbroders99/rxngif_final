fe33b1b00cac6757e367b8c85eb5ea5d1fe2c953
Raghu Betina <rbetina@gmail.com>
2013-11-22 11:08:11 -0600

    Adding privacy around users profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, :only => [:show, :edit, :update, :destroy]
-  before_action :authorize_user, :only => [:edit, :update, :destroy]
+  before_action :authorize_user, :only => [:show, :edit, :update, :destroy]
 
   def set_user
     @user = User.find(params[:id])
